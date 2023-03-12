de532dd2d5bd2ba6afa82db4255159f306ac7868
jassem toumi <jassemtoumi@dhcp-10-5-165-207.wireless.rochester.edu>
2023-03-11 20:59:26 -0500

    added an input-field that allows the user to enter a guess. also added check button that check the correctness of the answer and changes the color of the stats container to green if answer is correct and red if false

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         setIsFlipping(false);
       }, 700);
     }
+    clearInputField();
   };
   const handleBack = () => {
     if (currentCard > 0) {
@@ -31,6 +32,7 @@ function App() {
         setIsFlipping(false);
       }, 700);
     }
+    clearInputField();
   };
 
   const [isflipped, updateFlipState] = useState(false);
@@ -48,12 +50,18 @@ function App() {
     if (showSoccerCards === false && showTennisCards === true) {
       setShowSoccerCards(true);
       setShowTennisCards(false);
+      setCurrentStreak(0);
+      setLongestStreak(0);
+      clearInputField();
     }
   };
   const displayTennisCards = () => {
     if (showTennisCards === false && showSoccerCards === true) {
       setShowTennisCards(true);
       setShowSoccerCards(false);
+      setCurrentStreak(0);
+      setLongestStreak(0);
+      clearInputField();
     }
   };
 
@@ -61,16 +69,119 @@ function App() {
     setCurrentCard(0);
   }, [showSoccerCards] || [showTennisCards]);
 
-  document.body.style.backgroundImage= `url(${showSoccerCards ? soccerBackground : tennisBackground})`;
-  
+  document.body.style.backgroundImage = `url(${
+    showSoccerCards ? soccerBackground : tennisBackground
+  })`;
+
+  const preventD = (e) => {
+    e.preventDefault();
+  };
+  const [answer, setAnswer] = useState("");
+  const [prevAnswer, setPrevAnswer] = useState("");
+
+  const handleInputChange = (e) => {
+    setAnswer(e.target.value);
+  };
+
+  const clearInputField = () => {
+    setAnswer("");
+  };
+
+  const checkAnswer = () => {
+    setPrevAnswer(answer);
+    if (showSoccerCards) {
+      if (
+        soccerCards[currentCard].answer
+          .toLocaleLowerCase()
+          .includes(answer.toLocaleLowerCase()) &&
+        answer !== "" &&
+        prevAnswer !== answer
+      ) {
+        setCurrentStreak(currentStreak + 1);
+        showGreen();
+      } else {
+        if (prevAnswer != answer && answer !== "") {
+          if (currentStreak > longestStreak) {
+            setLongestStreak(currentStreak);
+          }
+          setCurrentStreak(0);
+          showRed();
+        }
+      }
+    } else if (showTennisCards) {
+      if (
+        tennisCards[currentCard].answer
+          .toLocaleLowerCase()
+          .includes(answer.toLocaleLowerCase()) &&
+        answer !== "" &&
+        prevAnswer !== answer
+      ) {
+        setCurrentStreak(currentStreak + 1);
+        showGreen();
+      } else {
+        if (prevAnswer != answer && answer !== "") {
+          if (currentStreak > longestStreak) {
+            setLongestStreak(currentStreak);
+          }
+          setCurrentStreak(0);
+          showRed();
+        }
+      }
+    }
+  };
+
+  const [currentStreak, setCurrentStreak] = useState(0);
+  const [longestStreak, setLongestStreak] = useState(0);
+  const [color, setColor] = useState("#ffffff");
+  const showGreen = () => {
+    setColor("#00ff00");
+    const timer = setTimeout(() => {
+      setColor("#ffffff");
+    }, 500);
+    return () => clearTimeout(timer);
+  };
+  const showRed = () => {
+    setColor("#ff0000");
+    const timer = setTimeout(() => {
+      setColor("#ffffff"); // Return the background color to white after 5ms
+      // setCorrect(''); // Reset the state variable to trigger the effect again
+    }, 500);
+    return () => clearTimeout(timer);
+  }
+
+  // impelementing Fisher-Yates shuffle algorithm
+  const shuffle = (array) => {
+    let currentIndex = array.length,
+      randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      //destructuring
+      [array[currentIndex], array[randomIndex]] = [ array[randomIndex], array[currentIndex] ];
+    }
+    return array;
+  }
+  const handleShuffle = () => {
+    if (showSoccerCards) {
+      shuffle(soccerCards);
+    } else if (showTennisCards) {
+      shuffle(tennisCards);
+    }
+  }
+
+
   return (
     <div className="App">
-      <div className="title">
+      <div className="title" style={{ backgroundColor: color }}>
         <h1>Guess Who?</h1>
         <h2>Test your knowledge in {showSoccerCards ? "soccer" : "tennis"}</h2>
         <h3>
           Number of cards: {showSoccerCards ? soccerArrLenght : tennisArrLength}
         </h3>
+        <div className="stats">
+          <h3>Current Streak: {currentStreak}</h3>
+          <h3>Longest Streak: {longestStreak}</h3>
+        </div>
       </div>
 
       <FlashCard
@@ -80,15 +191,33 @@ function App() {
         handleClick={handleClick}
         isflipping={isflipping}
       />
-      <button onClick={handleBack} className="nextBtn">
-        Back
-      </button>
-      <button onClick={handleNext} className="nextBtn">
-        Next
-      </button>
-      <div class="dropdown">
-        <button class="dropbtn">Select Topic</button>
-        <div class="dropdown-content">
+      <div className="form-container">
+        <form onSubmit={preventD}>
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Enter your answer"
+            value={answer}
+          />
+        </form>
+        <button className="checkBtn" onClick={checkAnswer}>
+          Check
+        </button>
+      </div>
+
+      <div className="btnS">
+        <button onClick={handleBack} className="nextBtn">
+          Back
+        </button>
+        <button onClick={handleNext} className="nextBtn">
+          Next
+        </button>
+      </div>
+      <button onClick={handleShuffle} className="shuffleBtn">Shuffle</button>
+
+      <div className="dropdown">
+        <button className="dropbtn">Select Topic</button>
+        <div className="dropdown-content">
           <span onClick={displaySoccerCards}>Soccer</span>
           <span onClick={displayTennisCards}>Tennis</span>
         </div>
